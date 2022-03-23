592c182644622fc1548a3fb4b430146799783fa3
ryanallenk <ryanallenk@gmail.com>
2022-03-23 16:08:32 -0400

    add login message

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 const PORT = 8080;
-const cookierParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
 function generateRandomString(){
   return Math.random().toString(36).slice(-6)
@@ -16,7 +16,7 @@ const urlDatabase = {
 };
 
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}), cookieParser());
 
 // in the event of a request type GET, if the route asked is "/", then run the callback
 // req is request and res is the response to send back
@@ -30,7 +30,10 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"] 
+  };
   res.render("urls_index", templateVars);
 });
 
@@ -56,11 +59,12 @@ app.post("/login", (req, res) => {
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
