5214c7a23dc19b856b0a23be51be7cde8b08df71
ryanallenk <ryanallenk@gmail.com>
2022-03-24 19:51:19 -0400

    add testing for helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,31 @@
+const { assert } = require('chai');
+
+const { findUserByEmail } = require('../helpers/userHelpers');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('findUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = findUserByEmail("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert(user.id === expectedUserID, `user.id is equal to 'userRandomID'`)
+  });
+  it('should return undefined if looking up an invalid email', function() {
+    const user = findUserByEmail("user9000@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert(user === undefined, `user is equal 'undefined'`)
+  });
+});
